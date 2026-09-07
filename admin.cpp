584ab75bff32a7ae39927496da1c6fd1103634d2
Anushka Sanewal <anushkasanewal@gmail.com>
2026-09-07 19:48:05 +0530

    Update admin.cpp

--- admin.cpp
+++ admin.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
+#include<string>
 using namespace std;
 class Admin{
     string Admin_password;
     string user_name;
+
+    const string ADMIN_USERNAME= "ADMIN";
+    const string ADMIN_PASS= "Hosttel@123";
+
+    cout<<"------HOSTEL MANAGEMENT SYSTEM------"<<endl;
+    cout<<"Admin login:"<<endl;
+
+    cout<<"Enter Admin Username: ";
+    cin>>user_name;
+    cout<<"Enter Admin Password: ";
+    cin>>Admin_password;
+
+    
+
+
 
 };
