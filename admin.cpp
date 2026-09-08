8c8eab964e25edbcacd4eaa6a6834defa696a52a
Hardika Joshi <joshihardika91@gmail.com>
2026-09-08 07:29:49 +0530

    Update admin.cpp
    admin login

--- admin.cpp
+++ admin.cpp
@@ -8,13 +8,13 @@ class Admin{
     const string ADMIN_USERNAME= "ADMIN";
     const string ADMIN_PASS= "Hosttel@123";
 
-    cout<<"------HOSTEL MANAGEMENT SYSTEM------"<<endl;
+    void login(){cout<<"------HOSTEL MANAGEMENT SYSTEM------"<<endl;
     cout<<"Admin login:"<<endl;
 
     cout<<"Enter Admin Username: ";
     cin>>user_name;
     cout<<"Enter Admin Password: ";
-    cin>>Admin_password;
+    cin>>Admin_password;}
 
     
 
