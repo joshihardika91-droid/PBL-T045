1debbc9ef6684b8710d865942d9444e21cfa5cd5
Code-Anshika <anshikashrinet2569@gmail.com>
2026-09-08 19:05:26 +0530

    Create PBL PHASE 1.cpp

---
+++ PBL PHASE 1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Attendance
+{
+public:
+    string StudentId;
+    string Name;
+    string Date;
+    string Status;
+void Attendancemark()
+{
+    cout << "Enter student id: " << endl;
+    cin >> StudentId;
+    cout << "Enter student name: " << endl;
+    cin.ignore();
+    getline(cin,Name);
+    cout << "Enter present date: " << endl;
+    cin >> Date;
+    cout << "Enter status (Present/Absent): " << endl;
+    cin >> Status;
+    cout << "Attendance marked successfully! " << endl;
+}
+};
+int main()
+{
+    Attendance at;
+    at.Attendancemark();
+    return 0;
+}
+
