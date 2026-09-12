97a0a97a5fc90393336244b4254ce5a99c3d58b7
Anushka Sanewal <anushkasanewal@gmail.com>
2026-09-12 20:21:57 +0530

    Update admin.cpp

--- admin.cpp
+++ admin.cpp
@@ -1,6 +1,36 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
+class Admin
+{
+    private:
+    string password;
+    string user;
+
+    const string admin_user= "ADMIN";
+    const string admin_pass= "Hosttel@123";
+    
+    public:
+    int login()
+    {
+    cout<<"------HOSTEL MANAGEMENT SYSTEM------"<<endl;
+    cout<<"Admin login:"<<endl;
+
+    cout<<"Enter Admin Username: ";
+    cin>>user;
+    cout<<"Enter Admin Password: ";
+    cin>>password;
+
+    if(user==admin_user && password==admin_pass)
+    return(1);
+    else
+    return(0);
+    }
+
+};
+
+
 class Attendance
 {
 private:
@@ -25,30 +55,20 @@ void Attendancemark()
 };
 int main()
 {
-    Attendance at;
-    at.Attendancemark();
+    Admin admin;
+
+         if(admin.login())
+    {
+        cout << "Login Successful!" << endl;
+
+        Attendance at;
+        at.Attendancemark();
+    }
+    else
+    {
+        cout << "Invalid Credentials!" << endl;
+    }
+
+
     return 0;
 }
-
-
-using namespace std;
-class Admin{
-    string Admin_password;
-    string user_name;
-
-    const string ADMIN_USERNAME= "ADMIN";
-    const string ADMIN_PASS= "Hosttel@123";
-
-    void login(){cout<<"------HOSTEL MANAGEMENT SYSTEM------"<<endl;
-    cout<<"Admin login:"<<endl;
-
-    cout<<"Enter Admin Username: ";
-    cin>>user_name;
-    cout<<"Enter Admin Password: ";
-    cin>>Admin_password;}
-
-    
-
-
-
-};
